16e38817da72e7419f5a329e42dc3f7c148793a5
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-05-28 00:26:09 -0300

    login structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Home from './components/Home/Home'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/main.scss';
@@ -9,17 +9,17 @@ import './styles/main.scss';
 const App = () => {
   return (
     <div>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-      
-      <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
-    );
+  );
 };
 
 export default App;
