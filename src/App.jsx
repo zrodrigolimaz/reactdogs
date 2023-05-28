6ecfe2939602ac4cde941e3b82b218bf627cbbe1
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-05-28 00:56:24 -0300

    user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import './styles/main.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './styles/main.scss';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
