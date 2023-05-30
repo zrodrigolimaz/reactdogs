8d9b8627911af9c9de203f39f3bd1cbae47819f6
zrodrigolimaz <zrodrigolimaz@gmail.com>
2023-05-30 15:34:48 -0300

    photo comments e photo single page

--- src/components/Photo/PhotoComments.jsx
+++ src/components/Photo/PhotoComments.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
-import { UserContext } from '../../UserContext';
-import PhotoCommentsForm from './PhotoCommentsForm';
-import styles from './PhotoComments.module.css';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
+import { COMMENT_POST } from '../../Api';
+import styles from './PhotoCommentsForm.module.css';
 
-const PhotoComments = (props) => {
-  const [comments, setComments] = React.useState(() => props.comments);
-  const commentsSection = React.useRef(null);
-  const { login } = React.useContext(UserContext);
+const PhotoCommentsForm = ({ id, setComments, single }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
 
-  React.useEffect(() => {
-    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
-  }, [comments]);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
 
   return (
-    <>
-      <ul ref={commentsSection} className={styles.comments}>
-        {comments.map((comment) => (
-          <li key={comment.comment_ID}>
-            <b>{comment.comment_author}: </b>
-            <span>{comment.comment_content}</span>
-          </li>
-        ))}
-      </ul>
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
-    </>
+    <form
+      className={`${styles.form} ${single ? styles.single : ''}`}
+      onSubmit={handleSubmit}
+    >
+      <textarea
+        className={styles.textarea}
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
   );
 };
 
-export default PhotoComments;
+export default PhotoCommentsForm;
